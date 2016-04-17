b4ad308c87b3ef1761d9f48e47cd1555b62326aa
Sundari Selvarajan <sundari_selva@hotmail.com>
2016-04-17 00:50:24 -0400

    with Question2

--- lab9.R
+++ lab9.R
@@ -1,4 +1,5 @@
 library(pROC)
+library(cvTools)
 data1 = read.csv("/Users/Sundu/Desktop/R/Work/train.csv")
 
 
@@ -21,3 +22,22 @@ plot(g)
 pt = predict(model,newdata = train , type='response')
 g = roc(TARGET~pt,data=train)
 plot(g)
+
+
+
+#####2###
+install.packages("cvTools")
+mm=cvFit(model,data=train,y=fitted.results,K=5)
+
+print(mm)
+install.packages("caret")
+
+train_control = trainControl(method="cv",number=5)
+grid = expand.grid(.fL=c(0), .usekernel=c(FALSE))
+model = train(TARGET~.,data=data1,trControl=train_control,method="nb",tuneGrid=grid)
+
+print(model)
+
+cvpred = predict(model,newdata=train, type='response')
+fitted.results <- ifelse(fitted.results > 0.5,1,0);
+misClasificError <- mean(fitted.results != test$TARGET);paste('Accuracy',1-misClasificError)
